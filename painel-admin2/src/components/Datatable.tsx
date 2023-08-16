feec79ceba77a41130449d884db9cab89db1c218
Vinicius <vinicius2335@gmail.com>
2023-08-16 17:49:45 -0300

    painel 2 - tentativa 3.1

--- painel-admin2/src/components/Datatable.tsx
+++ painel-admin2/src/components/Datatable.tsx
@@ -11,6 +11,7 @@ import {
 } from "@phosphor-icons/react"
 import { Menu, Transition } from "@headlessui/react"
 import { Fragment } from "react"
+import { ColunaOpcoes } from "./ColunaOpcoes"
 
 export function DataTable() {
   return (
@@ -49,152 +50,13 @@ export function DataTable() {
                 Add product
               </button>
 
-              {/* Botão de Açoes */}
-              <div className="flex items-center space-x-3 w-full md:w-auto">
-                <Menu as="div" className="relative inline-block text-left">
-                  <Menu.Button className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-white/10 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-white/20">
-                    Actions
-                    <CaretDown className="ml-2 w-5 h-5" />
-                  </Menu.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items
-                      className={
-                        "absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-400 rounded-md bg-zinc-700 text-gray-400 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                      }
-                    >
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={`${
-                              active ? "hover:bg-zinc-600 text-white font-bold" : "bg-zinc-700"
-                            } flex w-full items-center px-2 py-2 text-sm`}
-                          >
-                            Mass Edit
-                          </a>
-                        )}
-                      </Menu.Item>
-
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={`${
-                              active ? "hover:bg-zinc-600 text-white font-bold" : "bg-zinc-700"
-                            } flex w-full items-center px-2 py-2 text-sm`}
-                          >
-                            Delete All
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-
-                {/* Botão Filter */}
-                {/* TODO Menu para o filtro */}
-                <button
-                  id="filterDropdownButton"
-                  data-dropdown-toggle="filterDropdown"
-                  className="w-full md:w-auto flex items-center justify-around py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-white/10 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-white/20"
-                  type="button"
-                >
-                  <Funnel className="h-4 w-4 mr-2 text-gray-400" />
-                  Filter
-                  <CaretDown className="ml-2 w-5 h-5" />
-                </button>
-                <div
-                  id="filterDropdown"
-                  className="z-10 hidden w-48 p-3 bg-white rounded-lg shadow dark:bg-gray-700"
-                >
-                  <h6 className="mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                    Choose brand
-                  </h6>
-                  <ul className="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
-                    <li className="flex items-center">
-                      <input
-                        id="apple"
-                        type="checkbox"
-                        value=""
-                        className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        htmlFor="apple"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-                      >
-                        Apple (56)
-                      </label>
-                    </li>
-                    <li className="flex items-center">
-                      <input
-                        id="fitbit"
-                        type="checkbox"
-                        value=""
-                        className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        htmlFor="fitbit"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-                      >
-                        Microsoft (16)
-                      </label>
-                    </li>
-                    <li className="flex items-center">
-                      <input
-                        id="razor"
-                        type="checkbox"
-                        value=""
-                        className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
-                        Razor (49)
-                      </label>
-                    </li>
-                    <li className="flex items-center">
-                      <input
-                        id="nikon"
-                        type="checkbox"
-                        value=""
-                        className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        htmlFor="nikon"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-                      >
-                        Nikon (12)
-                      </label>
-                    </li>
-                    <li className="flex items-center">
-                      <input
-                        id="benq"
-                        type="checkbox"
-                        value=""
-                        className="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-                      />
-                      <label
-                        htmlFor="benq"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100"
-                      >
-                        BenQ (74)
-                      </label>
-                    </li>
-                  </ul>
-                </div>
-              </div>
             </div>
           </div>
 
+          {/* Tabela */}
           <div className="overflow-x-auto">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+
               {/* Cabeçalho da tabela*/}
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-white/10 dark:text-gray-400">
                 <tr>
@@ -234,53 +96,11 @@ export function DataTable() {
                   <td className="px-4 py-3">$2999</td>
                   <td className="px-4 py-3 flex items-center justify-end">
                     {/* TODO menu de açoes */}
-                    <button
-                      id="apple-imac-27-dropdown-button"
-                      data-dropdown-toggle="apple-imac-27-dropdown"
-                      className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                      type="button"
-                    >
-                      <DotsThree className="w-5 h-5" />
-                    </button>
-
-                    <div
-                      id="apple-imac-27-dropdown"
-                      className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                    >
-                      <ul
-                        className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                        aria-labelledby="apple-imac-27-dropdown-button"
-                      >
-                        <li>
-                          <a
-                            href="#"
-                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                          >
-                            Show
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="#"
-                            className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                          >
-                            Edit
-                          </a>
-                        </li>
-                      </ul>
-                      <div className="py-1">
-                        <a
-                          href="#"
-                          className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Delete
-                        </a>
-                      </div>
-                    </div>
+                    <ColunaOpcoes />
                   </td>
                 </tr>
 
-                <tr className="border-b border-gray-700 dark:hover:bg-white/10">
+                <tr className="border-b dark:border-white/10 hover:bg-gray-100 dark:hover:bg-white/10">
                   <th
                     scope="row"
                     className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
@@ -340,79 +160,16 @@ export function DataTable() {
             </table>
           </div>
 
-          {/* Paginação */}
+          {/* Footer da tabela */}
           <nav
-            className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+            className="flex justify-center items-center space-y-3 md:space-y-0 p-4"
             aria-label="Table navigation"
           >
             <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-              Showing
-              <span className="font-semibold text-gray-900 dark:text-white mx-2">1-10</span>
-              of
-              <span className="font-semibold text-gray-900 dark:text-white mx-2">1000</span>
+              Total de Produtos:
+              <span className="font-semibold text-gray-900 dark:text-white mx-2">50</span>
             </span>
-            <ul className="inline-flex items-stretch -space-x-px">
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-white/10 dark:hover:text-white"
-                >
-                  <span className="sr-only">Previous</span>
-                  <CaretLeft className="w-5 h-5" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-zinc-700 dark:hover:text-white"
-                >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-zinc-700 dark:hover:text-white"
-                >
-                  2
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  aria-current="page"
-                  className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-zinc-700 dark:bg-zinc-700 dark:text-white"
-                >
-                  3
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-zinc-700 dark:text-gray-400 dark:hover:bg-zinc-700 dark:hover:text-white"
-                >
-                  ...
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-zinc-700 dark:text-gray-400 dark:hover:bg-zinc-700 dark:hover:text-white"
-                >
-                  100
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-zinc-800 dark:border-zinc-700 dark:text-gray-400 dark:hover:bg-zinc-700 dark:hover:text-white"
-                >
-                  <span className="sr-only">Next</span>
-                  <CaretRight className="w-5 h-5" />
-                </a>
-              </li>
-            </ul>
-          </nav>
+            </nav>
         </div>
       </div>
     </section>
