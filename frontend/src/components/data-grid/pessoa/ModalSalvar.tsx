ab1b54952850bc1b7fb6da16c859ce812e189e2c
Vinicius Vieira <vinicius2335@gmail.com>
2023-09-11 13:43:08 -0300

    test(front): teste input mask + formik + nextui

--- frontend/src/components/data-grid/pessoa/ModalSalvar.tsx
+++ frontend/src/components/data-grid/pessoa/ModalSalvar.tsx
@@ -3,7 +3,7 @@ import { DataTypePermissao } from "@/models/permissao"
 import { DataTypePessoa, EnderecoType, createEmptyPessoa } from "@/models/pessoa"
 import { PermissaoService } from "@/services/PermissaoService"
 import { PessoaService } from "@/services/PessoaService"
-import { capitalize } from "@/services/utils"
+import { capitalize, regexNomeUtil } from "@/services/utils"
 import {
   Button,
   Input,
@@ -17,10 +17,11 @@ import {
 } from "@nextui-org/react"
 import { Certificate } from "@phosphor-icons/react"
 import { useFormik } from "formik"
-import React, { Key } from "react"
+import React from "react"
 import { toast } from "react-toastify"
 import * as yup from "yup"
 import { ViaCep } from "../../../models/ViaCep"
+import InputMask from "react-input-mask"
 
 interface ModalSalvarProps {
   isOpen: boolean
@@ -30,14 +31,16 @@ interface ModalSalvarProps {
   onSalvarPressed: () => void
 }
 
-const regexp = new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+const regexCpf = new RegExp(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+const regexCep = new RegExp(/^\d{5}-\d{3}$/)
+const regexNome = regexNomeUtil
 
 const validationSchema = yup.object({
-  nome: yup.string().required("Nome is required"),
-  cpf: yup.string().matches(regexp, "CPF is invalid").required("CPF is requided"),
+  nome: yup.string().matches(regexNome, "Nome is invalid. Ex: Maria Da Silva").required("Nome is required"),
+  cpf: yup.string().matches(regexCpf, "CPF is invalid. Ex: 999.999.999-99").required("CPF is requided"),
   email: yup.string().email("Email is invalid").required("Email is requided"),
   permissao: yup.string().required("Permissão is required"),
-  cep: yup.string().required("CEP is requided"),
+  cep: yup.string().matches(regexCep, "CEP is invalid. Ex: 99999-999").required("CEP is requided"),
   logradouro: yup.string().required("Logradouro is requided"),
   cidade: yup.string().required("Cidade is requided"),
   estado: yup.string().required("Estado is requided")
@@ -233,23 +236,32 @@ export default function ModalSalvar({
                         isRequired
                       />
 
-                      <Input
-                        autoFocus
-                        label="Cpf"
-                        id="cpf"
+                      <InputMask
+                        mask={"999.999.999-99"}
                         name="cpf"
-                        type="text"
-                        maxLength={14}
-                        placeholder="Digite o seu CPF..."
-                        variant="bordered"
+                        id="cpf"
                         value={formik.values.cpf}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        color={Boolean(formik.errors.cpf) ? "danger" : "success"}
-                        errorMessage={formik.errors.cpf}
-                        validationState={Boolean(formik.errors.cpf) ? "invalid" : "valid"}
-                        isRequired
-                      />
+                      >
+                        <Input
+                          autoFocus
+                          label="Cpf"
+                          //id="cpf"
+                          //name="cpf"
+                          type="text"
+                          //maxLength={14}
+                          placeholder="Digite o seu CPF..."
+                          variant="bordered"
+                          //value={formik.values.cpf}
+                          //onChange={formik.handleChange}
+                          //onBlur={formik.handleBlur}
+                          color={Boolean(formik.errors.cpf) ? "danger" : "success"}
+                          errorMessage={formik.errors.cpf}
+                          validationState={Boolean(formik.errors.cpf) ? "invalid" : "valid"}
+                          isRequired
+                        />
+                      </InputMask>
 
                       <Input
                         autoFocus
@@ -294,23 +306,26 @@ export default function ModalSalvar({
                     <div className="w-[50%] flex flex-col gap-3">
                       <h2>Endereço</h2>
 
-                      <Input
-                        autoFocus
-                        label="Cep"
+                      <InputMask
+                        mask={"99999-999"}
                         id="cep"
                         name="cep"
-                        type="text"
-                        maxLength={9}
-                        placeholder="Digite o seu cep..."
-                        variant="bordered"
                         value={formik.values.cep}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
-                        color={Boolean(formik.errors.cep) ? "danger" : "success"}
-                        errorMessage={formik.errors.cep}
-                        validationState={Boolean(formik.errors.cep) ? "invalid" : "valid"}
-                        isRequired
-                      />
+                      >
+                        <Input
+                          autoFocus
+                          label="Cep"
+                          type="text"
+                          placeholder="Digite o seu cep..."
+                          variant="bordered"
+                          color={Boolean(formik.errors.cep) ? "danger" : "success"}
+                          errorMessage={formik.errors.cep}
+                          validationState={Boolean(formik.errors.cep) ? "invalid" : "valid"}
+                          isRequired
+                        />
+                      </InputMask>
 
                       <Input
                         autoFocus
